7f7606546545dbfd0650b3009b2832762e9b4f6b
Vanessa Choy <vanessa.choy.robles@gmail.com>
2026-05-12 09:26:00 -0500

    feat: feat shared

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './style.css' // <--- AGREGA ESTA LÍNEA AQUÍ
+import './style.css'
 
 
 const app = createApp(App)
