af7f0b093ceed61dd2fb271a4c1b33bdc43b8503
Vanessa Choy <vanessa.choy.robles@gmail.com>
2026-05-12 23:04:58 -0500

    feat: feat merge develop with search and catalog

--- src/main.js
+++ src/main.js
@@ -1,34 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import router from './router'
+import router from './router' // Usamos tu archivo router.js principal
+
+// Estilos
 import './style.css'
-
-
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { searchRoutes } from './Search-and-catalog/search-routes.js'
-
-
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
+
 app.mount('#app')
-
-
-const pinia = createPinia()
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/catalog' },
-    ...searchRoutes,
-  ],
-})
-
-createApp(App)
-    .use(pinia)
-    .use(router)
-    .mount('#app')
