9763b3bd14258319b5fdda47524e63850a515bce
Victor Espino <u202411567@upc.edu.pe>
2026-06-19 14:38:36 -0500

    Enhance service documentation for payment methods
    Add a comment to describe CRUD operations on PaymentMethod.

--- src/Payments/application/payment-method.service.js
+++ src/Payments/application/payment-method.service.js
@@ -1,6 +1,7 @@
 /**
  * Servicio de aplicación de métodos de pago.
  * Capa: Payments / application
+ * Gestiona operaciones CRUD sobre PaymentMethod via PaymentMethodApi.
  */
 import { PaymentMethodApi } from "../infrastructure/payment-method.api.js";
 import { PaymentMethod } from "../domain/model/payment-method.entity.js";
